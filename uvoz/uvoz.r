493cfcf024ef9d40727e3d686114102a0f1ca889
Zala Stopar <zala.stopar1@gmail.com>
2019-12-12 10:03:29 +0100

    funkcija za branje html

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,14 +1,26 @@
 # 2. faza: Uvoz podatkov
 
-filmi1 <- read_csv("IMDB_Top250Engmovies2_OMDB_Detailed.csv", na = "N/A")[,c(3, 5:7, 12, 13, 19, 34, 36)]
+filmi1 <- read_csv("podatki/IMDB_Top250Engmovies2_OMDB_Detailed.csv", na = "N/A")[,c(2:3, 5:7, 12, 13, 19, 21, 34, 36)]
+
 
 
 #colnames(filmi) <- c("a", "b")
 
 
-filmi2 <- read_csv("movies_budget.csv", na = "N/A")[,c(9)]
+filmi2 <- read_csv("podatki/movies_budget.csv", na = "N/A")[, 9]
 
-tabela <- merge(filmi1, filmi2)
+tabela <- cbind(filmi1, filmi2)
 
-link <- "https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc"
-stran <- html_
+filmi_gross <- NULL
+for (i in seq(1, 250, 50)) {
+  link <- sprintf("https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&start=%d", i)
+  stran <- read_html(link)
+  imdbID <- stran %>% html_nodes(xpath="//h3[@class='lister-item-header']/a") %>% html_attr("href") %>%
+    strapplyc("tt[^/]+") %>% unlist()
+  gross <- stran %>% html_nodes(xpath="//p[@class='sort-num_votes-visible']") %>%
+    lapply(. %>% html_nodes(xpath="./span[@name='nv'][2]") %>% html_text() %>% parse_number() %>%
+             { ifelse(is.null(.), NA, .) }) %>% unlist()
+  filmi_gross <- rbind(filmi_gross, data.frame(imdbID, gross, stringsAsFactors=FALSE))
+}
+filmi <- left_join(tabela, filmi_gross)
+
