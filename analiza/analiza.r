baed3da188a1f629fef79612c6e056c6f4c780a0
zalastopar <zala.stopar1@gmail.com>
2020-03-04 13:23:19 +0100

    popravila graf za naprednejšo analizo

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,19 +1,15 @@
 # 4. faza: Analiza podatkov
 
 #analiza - revenue,budget
-rev <- head(filmi[c(1, 8)], 30)
-colnames(rev)[2] <-"var"
-bud <- head(filmi[c(1, 7)], 30)
-colnames(bud)[2] <- "var"
 
 
-analiza1 <- ggplot(NULL, aes(Title, var)) + 
-  geom_bar(aes(fill="bud"), data = bud, alpha = 0.5, stat = "identity") + 
-  geom_bar(aes(fill="rev"), data = rev, alpha = 0.5, stat = "identity") + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
-  labs(y="") + scale_fill_discrete(name = "", labels = c("Budget", "Revenue"))
-  
-
+analiza1 <- ggplot(head(filmi, 100), aes(Title, group=1)) + 
+  geom_point(aes(y = Budget, color = "Budget")) +
+  geom_point(aes(y = Revenue, color = "Revenue")) + 
+  scale_colour_manual("", breaks = c("Budget", "Revenue"), values = c("red", "blue")) + 
+  theme(axis.text.x = element_blank()) + labs(x = "Movies") +
+  geom_smooth(aes(y = Budget), color = "red", span = 0.3, fill = "red", alpha = 0.2)+
+  geom_smooth(aes(y = Revenue), color = "blue", span = 0.3, fill = "blue", alpha = 0.2)
 
 
 
